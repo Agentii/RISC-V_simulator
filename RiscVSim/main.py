74312cc03db4f837d515823c34c535dace430050
Agentii <56613955+Agentii@users.noreply.github.com>
2022-12-02 11:32:27 +0100

    Out-commented printProgram

--- RiscVSim/main.py
+++ RiscVSim/main.py
@@ -5,5 +5,5 @@ path = r">>path to your binary file (.bin)<<"
 if __name__ == "__main__":
     sim = Simulator()
     sim.loadProgram(path)
-    sim.printProgram()  # uncomment to print the program (in binary and encoded to RISC-V operations)
+    # sim.printProgram()  # uncomment to print the program (in binary and encoded to RISC-V operations)
     sim.run()
